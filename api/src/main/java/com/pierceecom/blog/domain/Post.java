67fcc6efb89592cf9ca54d7fe4695a7f7153e8b3
Paweł Kalita <kali@ncdc.pl>
2017-08-30 09:32:13 +0200

    Moved domain to shared api project + added Jersey integration test

---
+++ api/src/main/java/com/pierceecom/blog/domain/Post.java
@@ -0,0 +1,62 @@
+package com.pierceecom.blog.domain;
+
+import javax.xml.bind.annotation.XmlAccessOrder;
+import javax.xml.bind.annotation.XmlAccessorOrder;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
+public class Post {
+
+    private String id;
+    private String title;
+    private String content;
+
+    public Post() {
+    }
+
+    public Post(String id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        return id.equals(post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ integration-test/src/test/java/com/pierceecom/blog/JerseyTestIntegr.java
@@ -0,0 +1,105 @@
+package com.pierceecom.blog;
+
+import com.pierceecom.blog.domain.Post;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JerseyTestIntegr {
+
+    private static final String ENDPOINT = "http://localhost:8080/blog-web/";
+    private static final String PATH = "posts";
+
+    @Test
+    public void t1_empty() {
+        List<Post> list = GET(200);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void t2_add() {
+        Post post = new Post("1", "title 1", "content 1");
+        POST(post, 201);
+        Post post2 = new Post("2", "title 2", "content 2");
+        POST(post2, 201);
+    }
+
+    @Test
+    public void t3_get_all() {
+        List<Post> list = GET(200);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void t4_get_single() {
+        Post post1 = GET("1", 200);
+        assertEquals("title 1", post1.getTitle());
+
+        Post post2 = GET("2", 200);
+        assertEquals("title 2", post2.getTitle());
+    }
+
+    @Test
+    public void t5_delete_all() {
+        DELETE("1", 200);
+
+        DELETE("2", 200);
+    }
+
+    @Test
+    public void t6_empty() {
+        List<Post> list = GET(200);
+        assertEquals(0, list.size());
+    }
+
+    private void DELETE(String id, int expectedResponse) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ENDPOINT).path(PATH).path(id);
+        Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
+        Response response = request.delete();
+
+        assertEquals(expectedResponse, response.getStatus());
+    }
+
+    private void POST(Post post, int expectedResponse) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ENDPOINT).path(PATH);
+        Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
+        Response response = request.post(Entity.entity(post, MediaType.APPLICATION_JSON_TYPE));
+
+        assertEquals(expectedResponse, response.getStatus());
+
+    }
+
+    private Post GET(String uri, int expectedResponse) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ENDPOINT).path(PATH).path(uri);
+        Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
+        Response response = request.get();
+        assertEquals(expectedResponse, response.getStatus());
+
+        Post post = response.readEntity(Post.class);
+
+        return post;
+    }
+
+    private List<Post> GET(int expectedResponse) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(ENDPOINT).path("posts");
+        Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
+        Response response = request.get();
+
+        List<Post> list = response.readEntity(List.class);
+
+
+        return list;
+    }
+}
